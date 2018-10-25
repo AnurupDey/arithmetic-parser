e5f3d69cc24c64e2f92347f48bfc772514f2ebf5
Anurup Dey <anu.rup.dey98@gmail.com>
2018-10-25 16:11:41 +0530

    Implemented Lexer. Generates Token Nicely.

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -4,60 +4,49 @@
 #include <vector>
 #include <cctype>
 
-enum parseState{
-    PSNONE,
-    PSNUMBER,
-    PSOPPERATION
+enum LexState{
+    LSNONE
 };
 
-std::vector<std::string> tokenize(std::string input){
-    std::vector<std::string> tokens;
-    parseState state = PSNONE;
+enum TokenType{
+    TTNONE,
+    TTNUMBER,
+    TTOPERATOR,
+    TTBRACKET
+};
+struct Token{
+    Token(TokenType _type, std::string _value){
+        type = _type;
+        value = _value;
+    }
+
+    TokenType type;
+    std::string value;
+};
+
+std::string getNumberValue(char** curChar){
+    std::string number = "";
+    while((**curChar)!='\0' && (isdigit(**curChar) || (**curChar) == '.')){
+        number += **curChar;
+        (*curChar)++;
+    }
+    (*curChar)--;
+    return number;
+}
+
+std::vector<Token> tokenize(std::string input){
+    std::vector<Token> tokens;
+    LexState state = LSNONE;
 
     if(input.length() == 0 ) return tokens;
-
-    unsigned int i = 0;
     
-    std::string curToken = "";
-    
-    while(i <= input.length()){
-        char curChar = input[i];
-        switch(state){
-            case PSNONE:{
-                if(isdigit(curChar)){
-                    state = PSNUMBER;
-                    if(curToken != ""){
-                        tokens.push_back(curToken);
-                        curToken = "";
-                    }
-                } else if (curChar == '+' || curChar == '-' || curChar == '*' || curChar == '/'){
-                    state = PSOPPERATION;
-                    if(curToken != ""){
-                        tokens.push_back(curToken);
-                        curToken = "";
-                    }
-                }else i++;
-            } break;
-            case PSNUMBER: {
-                if(isdigit(curChar)){
-                    curToken += curChar;
-                    i++;
-                } else {
-                    state = PSNONE;
-                    tokens.push_back(curToken);
-                    curToken = "";
-                } 
-            } break;
-            case PSOPPERATION:{
-                if (curChar == '+' || curChar == '-' || curChar == '*' || curChar == '/'){
-                    curToken += curChar;
-                    i++;
-                } else {
-                    state = PSNONE;
-                    tokens.push_back(curToken);
-                    curToken = "";
-                }
-            } break;
+    for(char* curChar = &input[0]; *curChar != '\0'; curChar++){
+        if(isdigit(*curChar)){
+            tokens.emplace_back(TTNUMBER,getNumberValue(&curChar));
+        } else if (*curChar == '+' || *curChar == '-' || *curChar == '*' || *curChar == '/'){
+            tokens.emplace_back(TTOPERATOR,std::string("")+(*curChar));
+        } else if (*curChar == '(' || *curChar == ')'){
+            tokens.emplace_back(TTBRACKET,std::string("")+(*curChar));
         }
     }
 
@@ -76,9 +65,9 @@ int main(){
     in.getline(buffer,1024,';');
     std::string input = buffer;
     std::cout << "Buffer: " << buffer << std::endl;
-    std::vector<std::string> tokens = tokenize(input);
+    std::vector<Token> tokens = tokenize(input);
     for(auto& token : tokens){
-        std::cout << token << std::endl;
+        std::cout << '[' << token.type << ',' << token.value  << ']' << std::endl;
     }
 
     in.close();
